b1d16af2cd0efdf28c462e4d5ddd46fb7c2b2040
Renê Nunes Colombaro <rene.colombaro@hotmail.com>
2022-10-12 14:43:04 -0300

    feat: get url params

--- src/main/adapters/express-route-adapters.ts
+++ src/main/adapters/express-route-adapters.ts
@@ -3,7 +3,13 @@ import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const httpResponse = await controller.handle(req.body)
+    const accessToken = req?.headers['x-access-token']
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {}),
+      accessToken
+    }
+    const httpResponse = await controller.handle(request)
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }
