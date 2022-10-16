2a69d864b9703422c605beb888982d0de4ff4cce
Renê Nunes Colombaro <rene.colombaro@hotmail.com>
2022-10-16 16:43:28 -0300

    feat: join every user to specific room

--- src/presentation/controllers/web-socket/connection-controller.ts
+++ src/presentation/controllers/web-socket/connection-controller.ts
@@ -4,6 +4,7 @@ import { LoadAllRoomsIds } from '../../../domain/usecases/rooms/db-load-all-room
 import { ok, serverError } from '../../helpers/http-helper'
 import { EventController } from '../../protocols/avent-controller'
 import { HttpResponse } from '../../protocols/http'
+import jwt from 'jsonwebtoken'
 
 export class ConnectionController implements EventController {
   constructor (
@@ -15,10 +16,15 @@ export class ConnectionController implements EventController {
     socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
   ): Promise<HttpResponse<any>> {
     try {
+      const { accessToken: { token } } = socket.handshake.auth
+
+      const { idUser } = jwt.decode(token) as { idUser: number }
+
       console.log('Something connected')
       const allRoomsIds = await this.dbLoadAllRoomsIds.loadAllRoomsIds()
       allRoomsIds.forEach(async (id) => {
         await socket.join(`chat/${id}`)
+        await socket.join(`user/${idUser}`)
       })
       return ok({})
     } catch (error) {
